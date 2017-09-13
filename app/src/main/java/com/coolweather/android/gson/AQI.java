16c2242a05e21ab60cb28d422c10439c19d733e6
Fish <quanjiewansui@163.com>
2017-09-13 17:09:55 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Fish on 2017/9/13.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
